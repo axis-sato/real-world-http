0a0bbfd848d00d77538d22bf12b839028b78b5f7
c8112002 <c8112002@gmail.com>
2017-07-12 23:08:00 +0900

    Add chap3

---
+++ chap3/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://localhost:18888")
+	if err != nil {
+		print(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		print(err)
+	}
+
+	log.Println(string(body))
+}
